9bf61f93f7d461a48eef75957142cc4c5066e0ea
zhangzhi <1523334128@qq.com>
2023-03-06 21:48:33 +0800

    train(): 数组
    都有点思路，但是没有考虑完整。

---
+++ src/AutumnPlan_2023/day_3_6/MinimumMoves2EqualArrayElements.java
@@ -0,0 +1,60 @@
+package AutumnPlan_2023.day_3_6;
+
+// @date 2023/3/6
+// @time 20:48
+// @author zhangzhi
+// @description LeetCode 453. 最小操作次数使数组元素相等
+// 当一个事件比较复杂，不好求得时候，我们通常可以考虑转化为：求其对立事件。 -- 概率论思想
+
+import java.util.Arrays;
+
+public class MinimumMoves2EqualArrayElements {
+
+    /**
+     * 能用，但是会超时
+     * @param nums
+     * @return
+     */
+    public static int minMoves(int[] nums) {
+        Arrays.sort(nums);
+        int n =  nums.length;
+        int max = nums[n - 1];
+        long sum = 0;
+        int count = 0;
+        for(int num : nums){
+            sum += num;
+        }
+        while(true){
+            if(sum % n == 0){
+                long div = sum / n;
+                if(div >= max){
+                    return count;
+                }
+            }
+            sum += n - 1;
+            count++;
+        }
+    }
+
+    /**
+     * n-1个元素加 1，等同于 剩下那个元素减 1。注意这个关键点！！
+     * 然后统计出每个元素和最下的那个元素的差值，就是最小次数
+     * @param nums
+     * @return
+     */
+    public static int minMoves2(int[] nums){
+        Arrays.sort(nums);
+        int min = nums[0];
+        int total = 0;
+        for(int num : nums){
+            total += num - min;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,99};
+        System.out.println(minMoves(nums));
+        System.out.println(minMoves2(nums));
+    }
+}
---
+++ src/AutumnPlan_2023/day_3_6/NonDecreasingArray.java
@@ -0,0 +1,61 @@
+package AutumnPlan_2023.day_3_6;
+
+// @date 2023/3/6
+// @time 21:11
+// @author zhangzhi
+// @description LeetCode 665. 非递减数列
+// 想到了一半，但是没有想到在找到不满足 nums[i] <= nums[i+1] 的时候，修改nums[i] = nums[i+1]，再检查一次数组，看是否是非递减序列
+// 同样，不满足时，复原，把 nums[i+1] = nums[i]，重复上次的操作。这样才能知道修改后数组是否为非递减序列。
+// 题解：https://leetcode.cn/problems/non-decreasing-array/solution/fei-di-jian-shu-lie-by-leetcode-solution-zdsm/
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class NonDecreasingArray {
+
+    private static boolean isSorted(int[] nums){
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] > nums[i + 1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkPossibility(int[] nums) {
+        for(int i = 0;i <= nums.length - 2; i++){
+            int x = nums[i];
+            int y = nums[i + 1];
+            if(x > y){
+                nums[i] = y;
+                if(isSorted(nums)){
+                    return true;
+                }
+                nums[i] = x;
+                nums[i + 1] = x;
+                return isSorted(nums);
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkPossibility2(int[] nums){
+        int count = 0;
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addLast(nums[0]);
+        for(int i = 1; i <= nums.length - 1; i++){
+            while(!deque.isEmpty() && deque.peekLast() > nums[i]){
+                count++;
+                deque.pollLast();
+            }
+            deque.addLast(nums[i]);
+        }
+        return count <= 1 ? true : false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{5,7,1,8};
+        //System.out.println(checkPossibility(nums));
+        System.out.println(checkPossibility2(nums));
+    }
+}
