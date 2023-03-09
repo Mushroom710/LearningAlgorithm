0658b14651c8c411f3cfd911c3b550a47449af89
zhangzhi <1523334128@qq.com>
2023-03-09 22:26:56 +0800

    train(): 数组
    心态爆炸。

---
+++ src/AutumnPlan_2023/day_3_9/SetMismatch.java
@@ -0,0 +1,31 @@
+package AutumnPlan_2023.day_3_9;
+
+// @date 2023/3/9
+// @time 22:20
+// @author zhangzhi
+// @description LeetCode 645. 错误的集合
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SetMismatch {
+
+    public int[] findErrorNums(int[] nums) {
+        int[] errorNums = new int[2];
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int i = 1; i <= n; i++) {
+            int count = map.getOrDefault(i, 0);
+            if (count == 2) {
+                errorNums[0] = i;
+            } else if (count == 0) {
+                errorNums[1] = i;
+            }
+        }
+        return errorNums;
+    }
+}
