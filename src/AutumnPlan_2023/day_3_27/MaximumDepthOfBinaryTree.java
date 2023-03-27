f24e1aff897e4d97f8e2ee1c2081a99bff1bb613
zhangzhi <1523334128@qq.com>
2023-03-27 22:05:10 +0800

    train(): 二叉树
    后序遍历简直是通吃。
    对于二叉树的题目，要分析清楚采用哪种遍历顺序和各种边界条件！

---
+++ src/AutumnPlan_2023/day_3_27/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,38 @@
+package AutumnPlan_2023.day_3_27;
+
+// @date 2023/3/27
+// @time 22:00
+// @author zhangzhi
+// @description LeetCode 104. 二叉树的最大深度
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaximumDepthOfBinaryTree {
+
+    /**
+     * 层序遍历可以轻松找出树的高度和深度。
+     */
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int depth = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            depth++;
+            for(int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                if(node.left != null){
+                    queue.add(node.left);
+                }
+                if(node.right != null){
+                    queue.add(node.right);
+                }
+            }
+        }
+        return depth;
+    }
+}
---
+++ src/AutumnPlan_2023/day_3_27/SymmetricTree.java
@@ -0,0 +1,77 @@
+package AutumnPlan_2023.day_3_27;
+
+// @date 2023/3/27
+// @time 21:57
+// @author zhangzhi
+// @description LeetCode 101. 对称二叉树
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class SymmetricTree {
+
+    /**
+     * 队列，其实换成其他的容器是一样的效果
+     */
+    public boolean isSymmetric2(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null){
+            return true;
+        }
+        queue.add(root.left);
+        queue.add(root.right);
+        while(!queue.isEmpty()){
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+            if(left == null && right == null){
+                continue;
+            }
+            if(left == null || right == null || (left.val != right.val)){
+                return false;
+            }
+            queue.add(left.left);
+            queue.add(right.right);
+            queue.add(left.right);
+            queue.add(right.left);
+        }
+        return true;
+    }
+
+    /**
+     * 递归法
+     */
+    private boolean isSym(TreeNode left, TreeNode right){
+        if(left == null && right == null){
+            return true;
+        }else if(left == null || right == null){
+            return false;
+        }
+        if(!isSym(left.left, right.right)){
+            return false;
+        }
+        if(!isSym(left.right, right.left)){
+            return false;
+        }
+        return left.val == right.val;
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        return isSym(root.left, root.right);
+    }
+}
