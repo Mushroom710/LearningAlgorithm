d1cf0ba48686e1088ff0d5028a4603844c4e00d6
zhangzhi <1523334128@qq.com>
2023-05-04 22:19:45 +0800

    train(): 每日一题

---
+++ src/AutumnPlan_2023/May/day_5_4/MaximumFruitsHarvestedAfterAtMostKSteps.java
@@ -0,0 +1,64 @@
+package AutumnPlan_2023.May.day_5_4;
+
+// @date 2023/5/4
+// @time 22:18
+// @author zhangzhi
+// @description 每日一题 LeetCode 2106. 摘水果
+public class MaximumFruitsHarvestedAfterAtMostKSteps {
+    public int maxTotalFruits(int[][] fruits, int startPos, int k) {
+        int n = fruits.length;
+        int[] sum = new int[n + 1];
+        int[] indices = new int[n];
+        sum[0] = 0;
+        for (int i = 0; i < n; i++) {
+            sum[i + 1] = sum[i] + fruits[i][1];
+            indices[i] = fruits[i][0];
+        }
+        int ans = 0;
+        for (int x = 0; x <= k / 2; x++) {
+            /* 向左走 x 步，再向右走 k - x 步 */
+            int y = k - 2 * x;
+            int left = startPos - x;
+            int right = startPos + y;
+            int start = lowerBound(indices, 0, n - 1, left);
+            int end = upperBound(indices, 0, n - 1, right);
+            ans = Math.max(ans, sum[end] - sum[start]);
+            /* 向右走 x 步，再向左走 k - x 步 */
+            y = k - 2 * x;
+            left = startPos - y;
+            right = startPos + x;
+            start = lowerBound(indices, 0, n - 1, left);
+            end = upperBound(indices, 0, n - 1, right);
+            ans = Math.max(ans, sum[end] - sum[start]);
+        }
+        return ans;
+    }
+
+    public int lowerBound(int[] arr, int left, int right, int val) {
+        int res = right + 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] >= val) {
+                res = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+
+    public int upperBound(int[] arr, int left, int right, int val) {
+        int res = right + 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > val) {
+                res = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+}
